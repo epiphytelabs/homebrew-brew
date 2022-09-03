0f6e232d057ba516cb78c67ebcaff7e365684d7b
David Dollar <david@dollar.io>
2022-09-03 13:23:42 -0400

    keep 0.0.3

--- Formula/keep.rb
+++ Formula/keep.rb
@@ -1,8 +1,8 @@
 class Keep < Formula
   desc "Epiphyte Keep"
   homepage "https://epiphyte.xyz/"
-  url "https://github.com/epiphytelabs/keep/archive/v0.0.1.tar.gz"
-  sha256 "738f338c4f01b0eae2e90134f88b0c343202065916d438ed8368f791eb12e357"
+  url "https://github.com/epiphytelabs/keep/archive/v0.0.3.tar.gz"
+  sha256 "4058fd111122727e25059d6aecfb3de14b21aa61697a1bbdb5400aa93624ea54"
   license "GPL-3.0-or-later"
   version_scheme 1
 
