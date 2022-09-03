653c7a7a1ee055138c38ae614fb7a7726758a62b
David Dollar <david@dollar.io>
2022-09-03 13:13:28 -0400

    init

---
+++ Formula/keep.rb
@@ -0,0 +1,28 @@
+class Keep < Formula
+  desc "Epiphyte Keep"
+  homepage "https://epiphyte.xyz/"
+  url "https://github.com/epiphytelabs/keep/archive/v0.0.1.tar.gz"
+  sha256 "738f338c4f01b0eae2e90134f88b0c343202065916d438ed8368f791eb12e357"
+  license "GPL-3.0-or-later"
+  version_scheme 1
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+
+    system "go", "build", "-mod=vendor", *std_go_args(ldflags: ldflags), "./cmd/keep"
+  end
+
+  test do
+    assert_equal "dev\n", shell_output("#{bin}/keep version")
+  end
+end
